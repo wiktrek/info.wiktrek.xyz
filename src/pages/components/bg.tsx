d2e1db7426e303268776794a95ac1ee150618fdd
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-12-15 21:52:08 +0100

    sphere

--- src/pages/components/bg.tsx
+++ src/pages/components/bg.tsx
@@ -9,9 +9,25 @@ const BackGround = () => {
     const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
-      0.1,
+      0.01,
       1000
     );
+
+    function sphere(number: number) {
+      function random() {
+        return Math.floor(Math.random() * 30);
+      }
+      let a = 0;
+      while (a < number) {
+        const geometry = new THREE.SphereGeometry(4, 32, 16);
+        const material = new THREE.MeshNormalMaterial();
+        const sphere = new THREE.Mesh(geometry, material);
+        sphere.position.set(random(), random(), random());
+        scene.add(sphere);
+        a += 1;
+      }
+    }
+    sphere(15);
     const canvas = document.getElementById("ez");
     if (!canvas) return console.log("Can not find canvas");
     const renderer = new THREE.WebGLRenderer({ canvas });
