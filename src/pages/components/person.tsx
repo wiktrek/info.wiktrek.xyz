bd84052e41d66d7665d4edc52229ab45ab38ccc0
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-04 23:51:39 +0100

    person.tsx

--- src/pages/components/person.tsx
+++ src/pages/components/person.tsx
@@ -4,7 +4,17 @@ import Image from "next/image";
 const Person = () => {
   return (
     <div>
-
+      <div className="h-46 w-56 rounded-2xl bg-slate-800 text-white">
+        <a>Name</a>
+        <p>Last Name</p>
+        <Image
+          alt="info.wiktrek.xyz"
+          src="/image.png"
+          width="80"
+          height="50"
+          className="mx-auto"
+        />
+      </div>
     </div>
   );
 };
