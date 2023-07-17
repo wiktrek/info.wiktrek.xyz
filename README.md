6f816c677391727d9c28af010191fb71abaafa57
wiktrek <wiktor.janiszewski10@gmail.com>
2023-07-17 23:40:27 +0200

    added icon to _app

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # [affirmations api](https://www.affirmations.dev/)
 
-# []()
+# [dad joke api](https://icanhazdadjoke.com)
--- app/stuff/page.tsx
+++ app/stuff/page.tsx
@@ -1,13 +1,9 @@
-import Head from "next/head";
 import Link from "next/link";
 import Navbar from "~/components/navbar";
 
 export default function Page() {
   return (
     <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main className=" h-screen items-center justify-center text-center text-6xl  no-underline">
         <Navbar />
         <div className=" grid grid-cols-1">
