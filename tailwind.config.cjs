551a12992b00f4e405000c7ed5e5c9aab43d3581
wiktrek <wiktor.janiszewski10@gmail.com>
2024-01-28 22:19:36 +0100

    feat: api component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        text: "#fbfdfe",
+        background: "#05080a",
+        primary: "#6bc2a1",
+        secondary: "#1d0344",
+        accent: "#ef2572",
+      },
+    },
+  },
+  plugins: [],
+};
